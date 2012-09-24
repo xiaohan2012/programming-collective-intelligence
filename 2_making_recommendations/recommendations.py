a3f664ecdffc5bdbb12487467878dbd0244e9217
xiaohan2012 <xiaohan2012@gmail.com>
2012-09-24 10:08:13 +0800

    pearson correlation, cirtic similarity & item recommandation

--- 2_making_recommendations/recommendations.py
+++ 2_making_recommendations/recommendations.py
@@ -1,3 +1,11 @@
+"""
+by using euclidean distance and Pearson correlation to measure taste similarity,
+calculate the top `n` matches of a given person
+
+Note:
+metric space material
+http://en.wikipedia.org/wiki/Metric_space#Examples_of_metric_spaces
+"""
 from collections import defaultdict
 from numpy import *
 
@@ -30,7 +38,7 @@ def _to_defaultdict(prefs):
 
 critics = _to_defaultdict(critics)
 
-def sim_distance(prefs,person1,person2):
+def sim_euclidean(prefs,person1,person2):
     """using euclidean distance to measure the similarity between two users' tastes"""
     pp1 = prefs[person1]
     pp2 = prefs[person2]
@@ -43,13 +51,11 @@ def sim_distance(prefs,person1,person2):
 
 def _cov(v1,v2):
     v1 = array(v1);v2=array(v2)
-    #print "_cov",(v1-mean(v1)) * (v2-mean(v2))
-    return mean((v1-mean(v1)) * (v2-mean(v2)))
-
+    return mean(v1*v2) - mean(v1)*mean(v2)
 def _std_dev(v1):
-    return sqrt(sum((v1 - mean(v1))**2))
+    return sqrt(mean((v1 - mean(v1))**2))
 
-def pearson(prefs,person1,person2):
+def sim_pearson(prefs,person1,person2):
     """using pearson correlation to measure the similarity between two users' tastes"""
     pp1 = prefs[person1]
     pp2 = prefs[person2]
@@ -58,14 +64,42 @@ def pearson(prefs,person1,person2):
         return 0#they are totally different
     score1 = array([pp1[movie] for movie in movies])
     score2 = array([pp2[movie] for movie in movies])
-    print score1,score2
-    print _cov(score1,score2) , cov(score1,score2)
     return _cov(score1,score2) / ( _std_dev(score1) * _std_dev(score2) )
 
+def top_matches(person , prefs , n=5 , similarity = sim_pearson):
+    tastes = [ (similarity(prefs,person,other),other)
+                    for other in prefs if person != other]
+    tastes.sort(reverse = True)
+    return tastes[:n]
 
+def get_recommendations(person , prefs , similarity = sim_pearson):
+    #get movies `person` haven't seen
+    movies1 = set(movie for movie in prefs[person])
+    movies2 = set(movie for other in prefs if other != person for movie in prefs[other])
+    unseen_movies = movies2- movies1
+    sims = dict((other,similarity(prefs,person,other)) 
+                    for other in prefs if other != person)
+    print sims
+    like_rank = dict()
+    for movie in unseen_movies:
+        score_sum = 0.
+        sim_sum = 0.
+        for other in sims:
+            #ensure `other` has seen the movie 
+            if prefs[other][movie] !=0 and\
+                    sims[other] >= 0:#ensure the similarity is positive, meaning we are `alike`,at least to some extent
+                sim_sum += sims[other]
+                score_sum += prefs[other][movie] * sims[other]
+        #caculating the weighted average                
+        like_rank[movie] = score_sum/sim_sum
+    
+    return sorted(like_rank.iteritems() , key = lambda i:i[1], reverse = True)
+    #return sorted(like_rank , key = like_rank.__getitem__ , reverse = True)
 if __name__ == "__main__":
     #print sim_distance(critics,"Toby","Jack")
     #print sim_distance(critics,"Lisa","Gene")
-    print pearson(critics,"Lisa","Gene")
-    print pearson(critics,"Lisa","Lisa")
-
+    #print sim_pearson(critics,"Lisa","Gene")
+    #print pearson(critics,"Lisa","Lisa")
+    #print top_matches("Toby",critics,n=5,similarity = sim_pearson)
+    #print top_matches("Toby",critics,n=5,similarity = sim_euclidean)
+    print get_recommendations("Toby", critics, similarity = sim_pearson)
